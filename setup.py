2f90628e8b057368786493bf89504bbaf3a03637
moregale <moregale@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-12-09 10:18:36 +0000

    skeleton package for Eionet LDAP users editor
    git-svn-id: https://svn.eionet.europa.eu/repositories/Zope/trunk/eea.userseditor@19060 24821446-e7ea-0310-81ac-b189f4507b0b

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='eea.userseditor',
+      version='0.1',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+)
